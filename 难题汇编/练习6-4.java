3bcb4800dbe1157096512c086cc464c17ac91f1d
kbearwu <58018912+kbearwu@users.noreply.github.com>
2020-11-02 22:44:22 +0800

    Update 练习6-4.java

--- 难题汇编/练习6-4.java
+++ 难题汇编/练习6-4.java
@@ -15,25 +15,25 @@ public class ForthDay {
             a[i] = rand.nextInt(10) + 1;
         }
 
-        for (int i = 0;i < n; i++) {
+        for (int i = 0;i < n; i++) {     // 找出最大值，确定行数
             if ( a[i] >= max )
                     max = a[i];
         }
 
-        for (int j = 0; j < max;j++) {
+        for (int j = 0; j < max;j++) {          // 对每一行不同的数组元素确定输出*还是空格
             for (int i = 0; i < n; i++) {
                 if ( a[i] < max - j )
                         System.out.print("  ");
                 else
                     System.out.print("* ");
             }
-            System.out.println();
+            System.out.println();       //每一行结束后换行
         }
         for (int i = 0; i < n; i++)
-            System.out.print("--");
+            System.out.print("--");   //每一个数用两个-分隔
             System.out.println();
             for (int i = 0; i < n;i++)
-                System.out.print(i % 10 + " ");
+                System.out.print(i % 10 + " ");  //余数即为索引数
     }
 
 }
