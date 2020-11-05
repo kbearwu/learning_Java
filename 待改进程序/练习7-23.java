751c44ad08103512011b3b752edf73829a5ff85c
kbearwu <58018912+kbearwu@users.noreply.github.com>
2020-11-06 00:59:02 +0800

    Create 练习7-23.java

---
+++ 待改进程序/练习7-23.java
@@ -0,0 +1,32 @@
+//瑕疵：index数组创建时和a数组长度一致，导致实质结果后有0填补
+
+import java.util.Scanner;
+
+public class ForthDay {
+    static int[] arraySrchIdx(int[] a,int x) {
+        int[] index = new int[a.length];
+        for (int i = 0,n = 0; i < a.length; i++) {
+            if (a[i] == x) {
+                index[n] = i;
+                n++;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.print("数组的元素数量：");
+        int num = stdIn.nextInt();
+        int[] a = new int[num];
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("第" + (i + 1) + "个元素值：");
+            a[i] = stdIn.nextInt();
+        }
+        System.out.print("要查找的值：");
+        int value = stdIn.nextInt();
+        int[] idx = arraySrchIdx(a,value);
+        for (int i = 0; i < idx.length; i++)
+            System.out.println(idx[i]);
+    }
+}
